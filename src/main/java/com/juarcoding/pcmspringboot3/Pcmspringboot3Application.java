b35f74625d339700e42582a3097f9275a87961d1
pollchihuy <poll.chihuy@gmail.com>
2025-04-29 19:54:28 +0700

    Initial commit

---
+++ src/main/java/com/juarcoding/pcmspringboot3/Pcmspringboot3Application.java
@@ -0,0 +1,13 @@
+package com.juarcoding.pcmspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pcmspringboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pcmspringboot3Application.class, args);
+	}
+
+}
